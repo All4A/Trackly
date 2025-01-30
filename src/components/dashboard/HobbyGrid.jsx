112a4ac9f642683d4087b0afea02c71aad4e0a04
Alexander Ershov <ershovsanek.centr@yandex.ru>
2025-01-30 20:49:34 +0300

    feat: clickable cards

--- src/components/dashboard/HobbyGrid.jsx
+++ src/components/dashboard/HobbyGrid.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/HobbyGrid.css";
 
 export default function HobbyGrid({ hobbies }) {
+  const [hobbiesState, setHobbiesState] = useState(hobbies);
+
+  const handleCardClick = (id) => {
+    setHobbiesState((prevHobbies) =>
+      prevHobbies.map((hobby) =>
+        hobby.id === id ? { ...hobby, todayTime: hobby.todayTime + 1 } : hobby
+      )
+    );
+  };
+
   return (
     <div className="hobby-grid">
-      {hobbies.map((hobby) => (
-        <div key={hobby.id} className="hobby-card">
+      {hobbiesState.map((hobby) => (
+        <div
+          key={hobby.id}
+          className="hobby-card"
+          onClick={() => handleCardClick(hobby.id)}
+        >
           <div className="hobby-content">
             <div className="hobby-title">
               <span>Hobbie #{hobby.id}</span>
@@ -24,7 +38,12 @@ export default function HobbyGrid({ hobbies }) {
             <div className="hobby-progress">
               <span>Today: {hobby.todayTime} h</span>
               <div className="progress-bar">
-                <div className="progress" style={{ width: `${(hobby.todayTime / hobby.dailyGoal) * 100}%` }}></div>
+                <div
+                  className="progress"
+                  style={{
+                    width: `${(hobby.todayTime / hobby.dailyGoal) * 100}%`,
+                  }}
+                ></div>
               </div>
             </div>
           </div>
