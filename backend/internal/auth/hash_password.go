ba4163bd5dd35fe7a9f4a1f9d8a9055eeefd163f
Arseniy Chernykh <chernykharseniy18@mail.ru>
2025-01-31 12:06:53 +0300

    2

---
+++ backend/internal/auth/hash_password.go
@@ -0,0 +1,15 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func CheckPassword(hashedPassword, plainPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
