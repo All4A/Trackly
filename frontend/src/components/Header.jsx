2cea2b90f67a759127388e827e31e455e431b40c
Alexander Ershov <ershovsanek.centr@yandex.ru>
2025-02-02 14:31:33 +0300

    save header images locally

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <div className="logo-text">
           <img
             loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/ff468459569eb5cb75886dba6376949e254578ca5747be22f4dadce1a392ecc2?placeholderIfAbsent=true&apiKey=83f09f65141e45b4bd5a3a0e1157ad85"
+            src="trackly_logo.png"
             alt="Trackly logo"
           />
           <div>Trackly</div>
@@ -18,24 +18,24 @@ export default function Header() {
       <div className="overview">Overview</div>
       <div className="actions">
         <SearchBar className="search-bar" />
-        <button aria-label="Notifications" className="button-secondary">
+        <button aria-label="Settings" className="button-secondary">
           <img
             loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a81584adddc5b583c53a086198c572f0557904302b2b21ec8ac387f63007f91?placeholderIfAbsent=true&apiKey=83f09f65141e45b4bd5a3a0e1157ad85"
+            src="settings.png"
             alt=""
           />
         </button>
-        <button aria-label="Messages" className="button-secondary">
+        <button aria-label="Notifications" className="button-secondary">
           <img
             loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/992ab53975b6baad5692aff137133c86c9c7ad0d781f049045ca48b2f2d4b7cd?placeholderIfAbsent=true&apiKey=83f09f65141e45b4bd5a3a0e1157ad85"
+            src="notifications.png"
             alt=""
           />
         </button>
         <button aria-label="Profile" className="button-secondary">
           <img
             loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/71dfebeac48ab087439ad2d55a7c8824207c2c4935bd2f54058d9f94bd319dbc?placeholderIfAbsent=true&apiKey=83f09f65141e45b4bd5a3a0e1157ad85"
+            src="profile.png"
             alt=""
           />
         </button>
