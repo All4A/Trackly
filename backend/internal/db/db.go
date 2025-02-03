a76c2823344a33cf19e843e2a80c3c059b8149bc
Alexander Ershov <ershovsanek.centr@yandex.ru>
2025-02-03 17:34:27 +0300

    fix: add db init timeout

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -2,13 +2,15 @@ package db
 
 import (
 	"fmt"
+	"log"
+	"time"
+	"trackly-backend/internal/config"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	"trackly-backend/internal/config"
 )
 
 type scopeFn = func(db *gorm.DB) *gorm.DB
@@ -19,9 +21,22 @@ func InitDB(cfg *config.DbConfig) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DbName)
 	println("my db" + dsn)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
+	println("my db" + dsn)
+	retryConnectCount := 5
+	var db *gorm.DB
+	var conErr error
+	for i := 0; i < retryConnectCount; i++ {
+		database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			conErr = err
+			time.Sleep(5 * time.Second)
+		}
+		db = database
+		conErr = nil
+
+	}
+	if conErr != nil {
+		return nil, conErr
 	}
 
 	migraionDns := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DbName)
